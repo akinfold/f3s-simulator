ce8b5d976e7ecfda7d231c623ac5713d756b2383
Roman Akinfold <akinfold@gmail.com>
2012-08-26 20:02:30 +0800

    add gis to INSTALLED_APPS & ref to settings_local

--- f3s_simulator/settings.py
+++ f3s_simulator/settings.py
@@ -11,7 +11,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -115,6 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'django.contrib.gis',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -145,3 +146,5 @@ LOGGING = {
         },
     }
 }
+
+from settings_local import *
