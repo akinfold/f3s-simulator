cba8e62078d20de4e23ad25c20b11d0921026a36
Roman Akinfold <akinfold@gmail.com>
2012-08-28 00:52:19 +0800

    add Step model

--- f3s_simulator/apps/games/models.py
+++ f3s_simulator/apps/games/models.py
@@ -12,3 +12,20 @@ class Game(CommonModel):
     map = models.ForeignKey(Map, related_name='games')
     owner = models.ForeignKey(User, related_name='created_games')
     players = models.ManyToManyField(User, related_name='played_games')
+
+
+
+class Step(CommonModel):
+    """
+    Simulation step.
+    """
+    game = models.ForeignKey(Game, related_name='steps')
+    index = models.IntegerField(_('step number'), default=0, editable=False)
+
+    def __unicode__(self):
+        return '%d step of %s''s game on %s map' % (self.index, self.game.owner.username, self.game.map.name)
+
+    def save(self, force_insert=False, force_update=False, using=None):
+        steps = self.__class__.objects.filter(game_id=self.game_id).order_by('-index')
+        self.index = steps[0].index + 1 if steps else 0
+        super(self.__class__, self).save(force_insert, force_update, using)
