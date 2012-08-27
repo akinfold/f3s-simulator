88200b8f9ff3d3b1d405992bf40d1cfa10efe392
Roman Akinfold <akinfold@gmail.com>
2012-08-28 03:25:33 +0800

    move Map model to games app

--- f3s_simulator/apps/games/models.py
+++ f3s_simulator/apps/games/models.py
@@ -2,8 +2,23 @@
 from django.contrib.auth.models import User
 from django.contrib.gis.db import models
 from django.utils.translation import ugettext_lazy as _
-from f3s_simulator.apps.maps.models import Map
 from f3s_simulator.libs.models import CommonModel
+
+
+class Map(CommonModel):
+    """
+    Top level abstraction representing simulation initial state.
+    Some kind of level in terms of game dev.
+    """
+    name = models.CharField(_('name'), max_length=255, help_text=_('Map name'), unique=True)
+    author = models.ForeignKey(User, related_name='maps')
+    description = models.CharField(_('description'), max_length=1024, blank=True, default='',
+        help_text=_('Map description'))
+
+    def __unicode__(self):
+        return self.name
+
+
 
 class Game(CommonModel):
     """
--- f3s_simulator/apps/maps/models.py
+++ f3s_simulator/apps/maps/models.py
@@ -1,23 +1,8 @@
 # -*- coding: utf-8 -*-
-from django.contrib.auth.models import User
 from django.contrib.gis.db import models
 from django.utils.translation import ugettext_lazy as _
-from f3s_simulator.apps.games.models import Step
+from f3s_simulator.apps.games.models import Step, Map
 from f3s_simulator.libs.models import CommonModel
-
-class Map(CommonModel):
-    """
-    Top level abstraction representing simulation initial state.
-    Some kind of level in terms of game dev.
-    """
-    name = models.CharField(_('name'), max_length=255, help_text=_('Map name'), unique=True)
-    author = models.ForeignKey(User, related_name='maps')
-    description = models.CharField(_('description'), max_length=1024, blank=True, default='',
-        help_text=_('Map description'))
-
-    def __unicode__(self):
-        return self.name
-
 
 
 class Environment(CommonModel):
@@ -61,7 +46,7 @@ class Flammable(CommonModel):
     objects = models.GeoManager()
 
     def __unicode__(self):
-        return '%s on % map' % (self.name, self.map.name)
+        return '%s on %s map' % (self.name, self.map.name)
 
 
 
